df2ececcdf0930bd8abe15c9aef1cc98112ae8aa
IDGS-801-23001514 <yoceline58@hotmail.com>
2026-02-11 18:55:21 -0600

    Base de datos

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+from flask import g
+from config import DevelopmentConfig
+import forms
+from models import db, Alumnos
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf=CSRFProtect()
+
+
+@app.errorhandler(404)
+def pagina_no_encontrada(error):
+    return render_template("404.html"), 404
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template("alumnos.html")
 
 @app.route("/")
 @app.route("/index")
 def index():
 	return render_template("index.html")
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,20 @@
+from wtforms import Form
+from wtforms import StringField, IntegerField,PasswordField,FloatField, BooleanField
+from wtforms import EmailField
+from wtforms import validators
+
+
+class UserForm(Form):
+    id=IntegerField("id",
+                    [validators.number_range(min=1, max=20,message="valor no valido")])
+    nombre=StringField("nombre",[
+        validators.DataRequired(message="El nombre es requerido"),
+        validators.length(min=4,max=20,message="requiere min =4 y max = 20")
+    ])
+    apaterno=StringField("apaterno",[
+        validators.DataRequired(message="El apellido es requerido")
+    ])
+    email=EmailField("correo",[
+        validators.DataRequired(message="El correo es requerido"),
+        validators.Email(message="Ingrese un correo valido")
+    ])
